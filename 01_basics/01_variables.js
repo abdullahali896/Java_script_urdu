9a8a4ef31474f99e8e85d793f161156f441fe638
Anas Asif <a6525776@gmail.com>
2024-06-13 18:25:27 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Lahore"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "anas@gmail.com"
+accountPassword = "54321"
+accountCity = "Karachi"
+
+console.log(accountId);
+
+/*
+Prefer not to use var,
+because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
