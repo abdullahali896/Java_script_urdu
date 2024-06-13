70c6bec7520aece88033d0c2b59cc49559d0ce97
Anas Asif <a6525776@gmail.com>
2024-06-13 18:26:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "Anas"
+let age = 18
+let isloggedIn = true
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof age); // number
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
