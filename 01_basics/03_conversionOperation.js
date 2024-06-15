a2adce7887db8d01dd8cf47c936d6d3478223fbb
Anas Asif <a6525776@gmail.com>
2024-06-15 18:50:02 +0000

    video no. 7 conversion Operator

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,4 +22,32 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 // ******************** Operations *********************
 
- 
+ let value = 3
+ let negvalue = -value
+
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Anas"
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2); => 12
+// console.log(1 + "2"); => 12
+// console.log("1" + 2 +2); => 122
+// console.log(1 + 2 +"2"); => 32
+
+// console.log(true); => true
+// console.log(+true); => 1
+// console.log(+""); => 0
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
