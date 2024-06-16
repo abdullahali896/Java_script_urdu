58329b30d2d387c9ba8aff21047a3b8f4ce4ba2c
Anas Asif <a6525776@gmail.com>
2024-06-16 11:44:54 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,23 @@
+const name = "Anas"
+const repoCount = 50
+
+// console.log(name + repoCount);
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const myName = new String('Anas')
+// console.log(myName.toUpperCase());
+// console.log(myName.length);
+// console.log(myName[0]);
+// console.log(myName.charAt(3));
+// console.log(myName.indexOf('n'));
+
+const anotherName = "   Abdullah    "
+console.log(anotherName);
+console.log(anotherName.trim()); //trim function used for remove the spaces
+console.log(anotherName.replace('ll', 'L').trim()); 
+console.log(anotherName.includes('L')); 
+
+const gameName = "Hockey-Football-Cricket"
+
+console.log(gameName.split('-')); // used for converting string into array
