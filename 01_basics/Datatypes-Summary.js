d1654fb9812e7d92d04cb913771b5600c830299b
Anas Asif <a6525776@gmail.com>
2024-06-16 10:23:14 +0000

    Data type  Summary

---
+++ 01_basics/Datatypes-Summary.js
@@ -0,0 +1,44 @@
+// Primitive Data Type
+
+// 7 Types
+
+// 1. String
+// 2. Number
+// 3. Boolean 
+// 4. Null
+// 5. Undefined
+// 6. Symbol
+// 7. BigInt
+
+// Examples
+
+let name = "Anas"
+
+const score = 100
+const scoreValue = 100.2
+
+const isLoggedIn = false
+
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+const bigNumber = 347654786323726536278n
+
+// Reference Data type (non primitive)
+
+// Array,  Objects,  functions.
+
+const heros = ["Quaid e Azam", "Saddam Hussein"];
+
+let myObj = {
+    name: "Anas",
+    age: 18,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
