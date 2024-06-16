08132a7b75e73801386f568a9f0217a868ed1f72
Anas Asif <a6525776@gmail.com>
2024-06-16 09:48:47 +0000

    Comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+// *** This type of  comparisons can always put you in difficulty***
+
+// console.log(null == 0); => falsse
+// console.log(null > 0); => false
+// console.log(null >= 0); => true
+// console.log(undefined == 0); => false
+// console.log(undefined > 0); => false
+// console.log(undefined < 0); => false
+
+//                 *********************
+
+console.log("2" === 2); 
+console.log(2 === 2); 
