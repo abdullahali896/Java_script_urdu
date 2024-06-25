5b877f362c7dfd70dae9fcc2769680d0cb293783
Anas Asif <a6525776@gmail.com>
2024-06-25 05:22:42 +0000

    numbers

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,16 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+// console.log(balance.tofixed(1));   //Error 
+
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(3));
+
+const numbers = 1000000000
+console.log(numbers.toLocaleString());
+
+// +++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++
